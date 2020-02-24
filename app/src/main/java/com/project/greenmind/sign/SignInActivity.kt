eccc2ac768b14469d23774123d2f31d170d67e73
ksb0511 <soobin796@gmail.com>
2020-02-25 01:21:38 +0900

    first commit

---
+++ app/src/main/java/com/project/greenmind/sign/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.project.greenmind.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.greenmind.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
---
+++ app/src/main/java/com/project/greenmind/sign/SignUpFinishActivity.kt
@@ -0,0 +1,13 @@
+package com.project.greenmind.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.greenmind.R
+
+class SignUpFinishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_finish)
+    }
+}
