81d90ae51411fd649832c27cbe2a809d01ee3cc0
ksb0511 <soobin796@gmail.com>
2020-03-01 13:20:18 +0900

    home_recyclerview

--- app/src/main/java/com/project/greenmind/homeFragment/TestFragment.kt
+++ app/src/main/java/com/project/greenmind/homeFragment/TestFragment.kt
@@ -1,24 +1,43 @@
 package com.project.greenmind.homeFragment
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.project.greenmind.R
+import kotlinx.android.synthetic.main.fragment_test.*
 
 /**
  * A simple [Fragment] subclass.
  */
+// 아마 프래그먼트 안에 프래그먼트를 이용해야될 것 같음.
 class TestFragment : Fragment() {
+
+    private lateinit var next : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_test, container, false)
+
+/*
+
+        next = view.findViewById(R.id.test_next)
+        next.setOnClickListener {
+            val next = Intent(requireContext(), Test2Fragment::class.java)
+            startActivity(next)
+        }
+*/
+
+
+        return view
     }
 
 
