ac0918e4ee8ec3953ebb393ca5583c35b93511ea
SeoyeonKo <seoyeon99o@naver.com>
2020-03-11 22:50:37 +0900

    infomation/InfoDetailsActivity1추가/정보창구조수정/information 창 완료

--- app/src/main/java/com/project/greenmind/homeFragment/Infomation/InfoDetailsActivity2.kt
+++ app/src/main/java/com/project/greenmind/homeFragment/Infomation/InfoDetailsActivity2.kt
@@ -1,13 +1,11 @@
-package com.project.greenmind.homeFragment
+package com.project.greenmind.homeFragment.Infomation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.project.greenmind.R
 
-class InfoDetailsActivity1 : AppCompatActivity() {
+class InfoDetailsActivity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_info_details1)
     }
 }
---
+++ app/src/main/java/com/project/greenmind/homeFragment/Infomation/InfoDetailsActivity4.kt
@@ -0,0 +1,13 @@
+package com.project.greenmind.homeFragment.Infomation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.greenmind.R
+
+class InfoDetailsActivity4 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_details4)
+    }
+}
