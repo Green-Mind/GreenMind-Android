07711a2505bd365eca987d787b44ca9ad1217e78
SeoyeonKo <seoyeon99o@naver.com>
2020-03-19 19:25:39 +0900

    파베만 실패

---
+++ app/src/main/java/com/project/greenmind/homeFragment/test_views/TestActivity1_2.kt
@@ -0,0 +1,29 @@
+package com.project.greenmind.homeFragment.test_views
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.greenmind.R
+import kotlinx.android.synthetic.main.activity_test1_2.*
+import kotlinx.android.synthetic.main.activity_test2.*
+
+class TestActivity1_2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test1_2)
+
+        test1_2_next.setOnClickListener {
+            val intent = Intent(this, TestActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 2000){
+            //finish()
+            finishActivity(3000)
+        }
+    }
+}
